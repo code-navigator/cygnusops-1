ff0bb51957a3b22d9f09e2e53e37e91ebfc2ad91
Mark Dean <markbdean1@gmail.com>
2019-05-17 15:59:54 -0700

    Created basic gulpfile functionality.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,12 +1,118 @@
 'use strict';
+
 // Import configuration
 import config from './gulpfile.config.js'
 
-// Import Gulp
-import { dest, src } from 'gulp';
+// Import Gulp and plugins
+import { dest, parallel, series, src, watch } from 'gulp';    // Automate development workflow
+import sass from 'gulp-sass';                                 // Compile Sass files
+import cleanCss from 'gulp-clean-css';                        // Minify css files
+import concat from 'gulp-concat';                             // Concatenate files
+import htmlmin from 'gulp-htmlmin';                           // Minify html files
+import phpmin from 'gulp-php-minify';                         // Minify php files
+import gulpif from 'gulp-if';                                 // Conditional filter
 
+// Import general utilities
+import browserSync from 'browser-sync';                       // Live reloading of browser for testing
+import merge from 'merge-stream';                             // Merge an arbitrary number of streams
+import del from 'del';                                        // Delete files and folders
+import yargs from 'yargs';                                    // Parsing of command line arguments
+
+// Constants & Variables
+const PRODUCTION = yargs.argv.prod                            // True = production mode; false = development
+var server = browserSync.create();                            // Create instance of browser sync
+
+
+/**
+ * Copy files to WordPress theme.
+ * 
+ */
 export const copy = () => {
-  console.log('copy started');
   return src(config.srcCopyPath)
     .pipe(dest(config.destCopyPath));
 }
+
+export const clean = (done) => {
+  del(config.destPathFiles, { force: true });
+  done();
+}
+
+/**
+ * Merge css stylesheet with compiled scss.
+ *
+ */
+export const styles = () => {
+  var sassStream,
+      cssStream
+  
+  // CSS stylesheet
+  cssStream = src(config.srcPathCSS);
+
+  // Compile SCSS
+  sassStream = src(config.srcPathSCSS)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulpif(PRODUCTION, cleanCss({ compatibility: 'ie8' })));
+  
+  // Merge into one file and copy to theme
+  return merge(sassStream, cssStream)
+    .pipe(concat('style.css'))
+    .pipe(dest(config.destCopyPath));
+}
+
+/**
+ * Minify html and php files
+ *
+ */
+export const html = () => {
+  return src(config.srcPathTemplate)
+    .pipe(gulpif(PRODUCTION, htmlmin({ collapseWhitespace: true })))
+    .pipe(dest(config.destCopyPath));
+}
+
+export const php = () => {
+  return src(config.srcPathPHP)
+    .pipe(gulpif(PRODUCTION, phpmin()))
+    .pipe(dest(config.destCopyPath));
+}
+
+/**
+ * Reload browser.
+ *
+ * @param {function} done (call back function)
+ */
+export const reload = (done) => {
+  server.reload();
+  done();
+}
+
+
+/**
+ * Watch for changes to source files.
+ *
+ */
+export const watchForChange = () => {
+  // watch(config.srcCopyPath, series(styles, reload));
+  watch(config.srcPathSCSS, series(styles, reload));
+  watch(config.srcPathTemplate, series(html, reload));
+  watch(config.srcPathPHP, series(php, reload))
+}
+
+/**
+ * Start up server.
+ *
+ * @param {function} done (call back function)
+ */
+export const serve = (done) => {
+  server.init({
+    proxy: config.projectURL,
+    open: true
+  });
+  done();
+}
+
+/**
+ * Start up server and watch for changes to source files.
+ *
+ */
+export default series(clean, styles, html, php, serve, watchForChange);
+
--- gulpfile.config.js
+++ gulpfile.config.js
@@ -1,6 +1,12 @@
 const config = {
-  srcCopyPath:  ['./styles/css/style.css', './scripts/php/index.php'],
-  destCopyPath: '../../themes/cygnusops/'
+  srcCopyPath:        ['./styles/css/style.css'],
+  srcPathCSS:         ['./styles/css/*.css'],
+  srcPathSCSS:        ['./styles/scss/*.scss'],
+  srcPathTemplate:    ['./templates/*.php'],
+  srcPathPHP:         ['./scripts/php/*.php'],
+  destCopyPath:       ['../../themes/cygnusops/'],
+  destPathFiles:      ['../../themes/cygnusops/**.*'],
+  projectURL:         'http://dev.local'
 }
 
 export default config;
