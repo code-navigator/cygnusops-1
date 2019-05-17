e41179d625e62c07a1fe90e7f04dcadb76ad9ba2
Mark Dean <markbdean1@gmail.com>
2019-05-17 16:42:36 -0700

    Created the loop.

--- templates/index.php
+++ templates/index.php
@@ -6,6 +6,19 @@
   <title>Document</title>
 </head>
 <body>
-  Hello World
+
+<!-- The loop -->
+<?php while(have_posts()): ?>
+  
+  <?php the_post(); ?>
+    <h1>
+      <a href="<?php the_permalink(); ?>">
+        <?php the_title(); ?>
+      </a>
+    </h1>
+
+    <?php the_content(); ?>
+  <?php endwhile; ?>
+
 </body>
 </html>
