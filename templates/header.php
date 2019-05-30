9d5b2445ba6ab87cf927823d7565b94a26cea9ab
Mark Dean <markbdean1@gmail.com>
2019-05-29 18:12:20 -0700

    Added header.

--- templates/header.php
+++ templates/header.php
@@ -4,4 +4,14 @@
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-  <p>Header Content</p>
+  <header>
+    <div class="banner__full-image--aircraft">
+      <div class="block__brand">
+        <span class="large-text" >O<span class="no-spacing">P</span></span><span class="small-text">&</span><span class="large-text" >S</span>
+      </div>
+    </div>
+
+    <nav>
+
+    </nav>
+  </header>
