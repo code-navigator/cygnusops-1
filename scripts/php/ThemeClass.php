685177c694bbf30418806f7a75e27adae0969cfb
Mark Dean <markbdean1@gmail.com>
2019-05-31 16:56:33 -0700

    Add header image support.

--- scripts/php/ThemeClass.php
+++ scripts/php/ThemeClass.php
@@ -3,6 +3,13 @@
 class Theme {
   public function __construct() {
     $this->addStyle('theme-styles',  get_stylesheet_uri());
+    $this->addSupport('custom-header', array(
+      'default-text-color' => '000',
+      'width' => 1600,
+      'height' => 250,
+      'flex-width' => true,
+      'flex-height' => true
+    ));
   }
 
   public function addStyle($handle, $src = '', $deps = array(), $ver = false, $media = 'all') {
@@ -12,9 +19,26 @@ class Theme {
     return $this;
   }
 
+  public function addSupport($feature, $options = null) {
+    $this->actionAfterSetup(function() use ($feature, $options) {
+      if ($options) {
+        add_theme_support($feature, $options);
+      } else {
+        add_theme_support($feature);
+      }
+    });
+    return $this;
+  }
+
   private function actionEnqueueScripts($function) {
     add_action('wp_enqueue_scripts', function() use ($function) {
         $function();
+    });
+  }
+
+  private function actionAfterSetup($function) {
+    add_action('after_setup_theme', function() use ($function) {
+      $function();
     });
   }
 }
--- templates/header.php
+++ templates/header.php
@@ -5,7 +5,12 @@
 </head>
 <body <?php body_class(); ?>>
   <header>
-    <div class="banner__full-image--aircraft">
+    <div class="banner__full-image--aircraft" 
+      
+      <?php if (get_header_image() != '') : ?>
+        style="background-image: url(<?php header_image(); ?>);"
+      <?php endif; ?>>
+      
       <div class="block__brand">
         <span class="large-text" >O<span class="no-spacing">P</span></span><span class="small-text">&</span><span class="large-text" >S</span>
       </div>
