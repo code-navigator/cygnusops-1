e1e3b60d681eaf772a901c411c45bb605d9484db
code-navigator <markbdean1@gmail.com>
2019-06-02 20:01:34 -0700

    Add navigation bar.

--- scripts/php/ThemeClass.php
+++ scripts/php/ThemeClass.php
@@ -3,6 +3,10 @@
 class Theme {
   public function __construct() {
     $this->addStyle('theme-styles',  get_stylesheet_uri());
+    $this->addNavMenus([
+      'menu-1' => 'primary'
+    ]);
+    $this->addSupport('custom-header');
   }
 
   public function addStyle($handle, $src = '', $deps = array(), $ver = false, $media = 'all') {
@@ -12,8 +16,32 @@ class Theme {
     return $this;
   }
 
+  public function addNavMenus($locations = array()) {
+    add_action('after_setup_theme',function() use ($locations){
+        register_nav_menus($locations);
+    });
+    return $this;
+  }
+
+  public function addSupport($feature, $options = null) {
+    $this->actionAfterSetup(function() use ($feature, $options) {
+        if ($options){
+            add_theme_support($feature, $options);
+        } else {
+            add_theme_support($feature);
+        }
+    });
+    return $this;
+  }
+
   private function actionEnqueueScripts($function) {
     add_action('wp_enqueue_scripts', function() use ($function) {
+        $function();
+    });
+  }
+
+  private function actionAfterSetup($function) {
+    add_action('after_setup_theme', function() use ($function) {
         $function();
     });
   }
--- templates/header.php
+++ templates/header.php
@@ -1,17 +1,40 @@
 <html>
+
 <head>
   <title>My Title</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
   <?php wp_head(); ?>
 </head>
+
 <body <?php body_class(); ?>>
+
+  <!-- site-header -->
+
   <header>
-    <div class="banner__full-image--aircraft">
-      <div class="block__brand">
-        <span class="large-text" >O<span class="no-spacing">P</span></span><span class="small-text">&</span><span class="large-text" >S</span>
+    <div class="banner__full-image--aircraft" style="background-image: url(<?php header_image(); ?>);">
+      <div class="container">
+        <div class="block__brand">
+          <span class="large-text" >O<span class="no-spacing">P</span></span>
+          <span class="small-text">&</span>
+          <span class="large-text" >S</span>
+        </div>
+        <div class="tagline">
+          <?php bloginfo('description'); ?>
+        </div>
       </div>
     </div>
 
-    <nav>
-
+    <!-- navigation bar -->
+    
+    <nav class="navbar">
+      <?php 
+        wp_nav_menu( array( 'theme_location' => 'menu-1' ) );
+      ?>
     </nav>
+
+    <!-- /navigation-bar -->
+
   </header>
+
+  <!-- /site-header -->
